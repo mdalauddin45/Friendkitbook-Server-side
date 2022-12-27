c9e7b15da785440393ba116629d4a68cffdbddce
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-12-27 17:57:58 +0600

    postsCollection add

---
+++ index.js
@@ -0,0 +1,177 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Decode JWT
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    console.log(decoded);
+    req.decoded = decoded;
+    next();
+  });
+}
+
+//MongoDb Add
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.teba24n.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+// Connect to MongoDb
+async function run() {
+  try {
+    const postsCollection = client.db("friendkitbook").collection("posts");
+    const usersCollection = client.db("friendkitbook").collection("users");
+
+    // // Verify Admin
+    // const verifyAdmin = async (req, res, next) => {
+    //   const decodedEmail = req.decoded.email;
+    //   const query = { email: decodedEmail };
+    //   const user = await usersCollection.findOne(query);
+
+    //   if (user?.role !== "admin") {
+    //     return res.status(403).send({ message: "forbidden access" });
+    //   }
+    //   console.log("Admin true");
+    //   next();
+    // };
+
+    // Save user email & generate JWT
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      console.log(result);
+      res.send({ result, token });
+    });
+
+    // Get All User
+    app.get("/users", async (req, res) => {
+      const users = await usersCollection.find({}).toArray();
+      res.send(users);
+    });
+
+    // Get A Single User
+    app.get("/user/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const decodedEmail = req.decoded.email;
+
+      if (email !== decodedEmail) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      res.send(user);
+    });
+
+    // get all products
+    app.get("/products", async (req, res) => {
+      const product = await postsCollection.find({}).toArray();
+      res.send(product);
+    });
+
+    // Get All products for host
+    app.get("/products/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const decodedEmail = req.decoded.email;
+
+      if (email !== decodedEmail) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      const query = {
+        "seller.email": email,
+      };
+      const cursor = postsCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+
+    // Post A Product
+    app.post("/products", verifyJWT, async (req, res) => {
+      const product = req.body;
+      console.log(product);
+      const result = await postsCollection.insertOne(product);
+      res.send(result);
+    });
+
+    // Update A product
+    app.put("/products", verifyJWT, async (req, res) => {
+      const product = req.body;
+      console.log(product);
+
+      const filter = {};
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: product,
+      };
+      const result = await postsCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+
+    // Delete a product
+    app.delete("/product/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await postsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // Get Single product
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await postsCollection.findOne(query);
+      res.send(product);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+run().catch((err) => console.error(err));
+
+app.get("/", (req, res) => {
+  res.send("Friendkitbook Server is running...");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running...on ${port}`);
+});
